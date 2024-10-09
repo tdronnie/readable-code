cffe9b2953cba69e9e740f83799e55144a54b573
wbluke <pwb0127@gmail.com>
2024-10-09 21:44:20 +0900

    mission/Rename: mission 폴더 생성

---
+++ src/main/java/cleancode/studycafe/mission/StudyCafeApplication.java
@@ -0,0 +1,12 @@
+package cleancode.studycafe.mission;
+
+import cleancode.studycafe.tobe.StudyCafePassMachine;
+
+public class StudyCafeApplication {
+
+    public static void main(String[] args) {
+        StudyCafePassMachine studyCafePassMachine = new StudyCafePassMachine();
+        studyCafePassMachine.run();
+    }
+
+}
